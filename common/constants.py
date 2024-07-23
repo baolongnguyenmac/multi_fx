6b72d37c42f5967054891519cb70019f5ac90e92
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2024-07-23 09:49:25 +0900

    add some constants

--- common/constants.py
+++ common/constants.py
@@ -1,7 +1,15 @@
 import os
 
 # dirs
-RAW_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/raw_data')
+RAW_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/fx_data')
 RESULTS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../results')
 EXE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../execute')
 LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../execute/log')
+
+# mode
+REG = 'regression'
+CLF = 'classification'
+LSTM = 'lstm'
+CNN = 'cnn'
+LSTM_CNN = 'lstm+cnn'
+ATT = 'attention'
