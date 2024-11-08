b4ada55f95798d03069e198baeb49a24d265d7f0
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2024-11-08 14:45:34 +0900

    create class DataLoader

--- data/dataloader.py
+++ data/dataloader.py
@@ -2,11 +2,21 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
+import random
+random.seed(84)
 
 from common.constants import *
 
 class DataLoader:
-    def __init__( self, dataset:str, look_back:int=None, batch_size:int=32, test_size:float=0.8, mode:str=CLF) -> None:
+    def __init__(
+        self,
+        dataset:str,
+        look_back:int=None,
+        batch_size:int=32,
+        test_size:float=0.8,
+        mode:str=CLF,
+        is_multi:bool=False
+    ) -> None:
         """initialize for dataloader. in case of NHITS, we need nothing but the `dataset`
 
         Args:
@@ -22,6 +32,7 @@ class DataLoader:
         self.dataset = dataset
         self.data_dir = os.path.join(RAW_DATA_DIR, dataset)
         self.mode = mode
+        self.is_multi = is_multi
 
         # extract columns by reading 1 *.csv file
         # this is not used for NHITS, the one for NHITS is written at `get_data`
@@ -48,35 +59,19 @@ class DataLoader:
             label_col = (label_col>0)*1.
             label_col = np.append([-1], label_col) # the first value is assigned to -1, it's removed anyway
         elif self.mode == REG:
-            # if it is regression problem, we have to drop the label column in df
-            df = np.delete(df, label, axis=1)
+            # if it is regression problem, we don't have to to anything
+            pass
         return label_col
 
-    def _normalize_data(self, df:np.ndarray, label:int=-1) -> np.ndarray:
+    def _normalize_data(self, df:np.ndarray, label:int=-1):
         scaler = StandardScaler()
-        return scaler.fit_transform(df), scaler.mean_[label], scaler.scale_[label]
+        return (scaler.fit_transform(df), scaler.mean_[label], scaler.scale_[label]) if self.mode == REG else (scaler.fit_transform(df), None, None)
 
-    def _create_dataset(self, df:np.ndarray, label:int, include_current:bool=False)->tuple[np.ndarray, np.ndarray]:
-        """create dataset with lookback window
-        there are 3 scenarios:
-            [x] use `x_{t-L:t}` to predict `y_{t+1}` ----> since we have to control `x_{t+1}` so `y_{t+1}` can be a good value, i don't think we need this scenario
-            [v] use `x_t` to predict `y_t` ----> set `lookback=0` and `include_current=False`
-            [v] use `x_{t-L:t}` to predict `y_t` ----> set `include_current=True`
-        i have to manually config these scenarios in the code (instead of passing parameter)
-
-        Args:
-            df (np.ndarray): original dataset
-            label (int): index of label column
-            include_current (bool): True if we use `x_{t-L:t}` to predict `y_t`, default: False
-
-        Returns:
-            tuple[np.ndarray, np.ndarray]: X and y
-        """
-        include_current = False if self.look_back==0 else include_current
-        size = df.shape[0] - self.look_back + include_current
+    def _create_dataset(self, df:np.ndarray, label:int)->tuple[np.ndarray, np.ndarray]:
+        size = df.shape[0] - self.look_back
         y = self._create_label(df, label)
-        X = np.array([df[i : i+self.look_back+include_current] for i in range(size)])
-        y = y[self.look_back-include_current:]
+        X = np.array([df[i : i+self.look_back] for i in range(size)])
+        y = y[self.look_back:]
         return X, y.reshape(-1,1)
 
     def get_multi_data(self, label:int=-1) -> dict[str, dict]:
@@ -86,9 +81,9 @@ class DataLoader:
 
             label (int, optional): The label to be predict the movement. Defaults to -1 (the final feature).
         """
-        cid = 0
         data_dict:dict[str, dict[str, pd.DataFrame]] = {}
-        for file_name in os.listdir(self.data_dir):
+        list_file = os.listdir(self.data_dir)
+        for cid, file_name in enumerate(list_file):
             if file_name.endswith('.csv'):
                 # read dataset, drop the datetime/index column
                 df = pd.read_csv(os.path.join(self.data_dir, file_name)).to_numpy()[:,1:]
@@ -97,20 +92,61 @@ class DataLoader:
                 transformed_df, mean_, std_ = self._normalize_data(df, label)
 
                 # create dataset with lookback window
-                X, y = self._create_dataset(transformed_df, self.look_back, label)
+                X, y = self._create_dataset(df=transformed_df, label=label)
 
-                tmp_dict = {}
-                tmp_dict['support_X'], tmp_dict['query_X'], tmp_dict['support_y'], tmp_dict['query_y'] = train_test_split(X, y, test_size=self.test_size, shuffle=False)
-                if self.batch_size != -1: # do not divide data into batches
-                    for key in tmp_dict.keys():
-                        num_batch = int(np.ceil(tmp_dict[key].shape[0]/self.batch_size))
-                        tmp_dict[key] = np.array_split(tmp_dict[key], num_batch)
-                tmp_dict['mean'] = mean_
-                tmp_dict['std'] = std_
-                data_dict[cid] = tmp_dict
-                cid += 1
+                if self.is_multi:
+                    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.5, shuffle=False)
+
+                    tmp_train_dict = self.split_support_query(train_X, train_y, mean_, std_)
+                    tmp_test_dict = self.split_support_query(test_X, test_y, mean_, std_)
+
+                    data_dict[cid] = tmp_train_dict
+                    data_dict[cid+len(list_file)] = tmp_test_dict
+                else:
+                    tmp_dict = self.split_support_query(X, y, mean_, std_)
+                    data_dict[cid] = tmp_dict
 
         return data_dict
+
+    def split_support_query(self, df_X:pd.DataFrame, df_y:pd.DataFrame, mean_:float=None, std_:float=None):
+        """split df into support and query set, by batch
+
+        Args:
+            df_X (pd.DataFrame): original df
+            df_y (pd.DataFrame): original df
+            mean_ (float, optional): mean of label (only used in case of regression). Defaults to None.
+            std_ (float, optional): std of label (only used in case of regression). Defaults to None.
+
+        Returns:
+            dict[str, something]: contains support and query data, mean and std sometime
+        """
+        data_dict:dict[str:pd.DataFrame] = {}
+        data_dict['support_X'], data_dict['query_X'], data_dict['support_y'], data_dict['query_y'] = train_test_split(df_X, df_y, test_size=self.test_size, shuffle=False)
+        if self.batch_size != -1: # divide data into batches
+            for key in data_dict.keys():
+                num_batch = int(np.ceil(data_dict[key].shape[0]/self.batch_size))
+                data_dict[key] = np.array_split(data_dict[key], num_batch)
+        data_dict['mean'] = mean_
+        data_dict['std'] = std_
+        return data_dict
+
+    def get_list_id(self, list_id:list[int]):
+        """get the list_id_train, list_id_val, list_id_test for  running stuff
+        """
+        list_id = sorted(list_id)
+        list_id_train = list_id[:int(len(list_id)*0.5)]
+        if self.is_multi:
+            # only apply for multi_fx
+            # choose the first 50% tasks for training, the last 50% is randomly chosen for validating and testing
+            list_id = set(list_id)
+            list_id_val = random.sample(sorted(list_id.difference(set(list_id_train))), len(list_id)//4)
+            list_id_test = list(list_id.difference(set(list_id_train).union(list_id_val)))
+        else:
+            # bởi vì các dataset khác không multi
+            # nên phải đảm bảo tính thứ tự trong huấn luyện
+            list_id_val = list_id[int(len(list_id)*0.5):int(len(list_id)*0.75)]
+            list_id_test = list_id[int(len(list_id)*0.75):]
+        return list_id_train, list_id_val, list_id_test
 
     def get_data(self, label:int=-1) -> tuple[pd.DataFrame, float, float]:
         """
@@ -124,16 +160,7 @@ class DataLoader:
             # data_path should contain only 1 *.csv file
             if file_name.endswith('.csv'):
                 # read dataset, drop the datetime/index column
-                df = pd.read_csv(os.path.join(data_path, file_name))
-
-                # save the columns to config df for NHITS
-                if self.mode == CLF:
-                    columns = list(df.columns[1:])
-                elif self.mode == REG:
-                    columns = list(df.columns[1:])
-                    columns.pop(label)
-
-                df = df.to_numpy()[:,1:]
+                df = pd.read_csv(os.path.join(data_path, file_name)).to_numpy()[:,1:]
 
                 # normalize data
                 transformed_df, mean_, std_ = self._normalize_data(df, label)
@@ -143,7 +170,7 @@ class DataLoader:
 
                 # concat X, y
                 transformed_df = np.concatenate([transformed_df, y.reshape(-1,1)], axis=1)
-                transformed_df = pd.DataFrame(transformed_df, columns=columns+['y'])
+                transformed_df = pd.DataFrame(transformed_df, columns=self.columns+['y'])
                 transformed_df['unique_id'] = 0
                 transformed_df['ds'] = pd.date_range(start='2000-01-01', periods=len(transformed_df), freq='h') # re-index all data
                 return transformed_df, mean_, std_
--- execute/main.py
+++ execute/main.py
@@ -3,7 +3,6 @@ import random
 random.seed(84)
 import argparse
 import json
-import numpy as np
 
 from model.meta_model import MAML
 from common.constants import *
@@ -14,49 +13,43 @@ def parse_args():
     parser.add_argument('-window', '--look_back_window', type=int, required=True)
     parser.add_argument('-model', '--based_model', type=str, choices=[LSTM, CNN, LSTM_CNN, ATT], default=LSTM, required=False)
     parser.add_argument('-mode', '--mode', type=str, choices=[REG, CLF], default=CLF, required=True)
+    parser.add_argument('-out_shape', '--output_shape', type=int, default=2, required=True)
     parser.add_argument('-inner_lr', '--inner_learning_rate', type=float, required=True) # we need this param in test phase
     parser.add_argument('-outer_lr', '--outer_learning_rate', type=float, default=0.001, required=False)
     parser.add_argument('-bt_size', '--batch_task_size', type=int, default=5, required=False)
     parser.add_argument('-rounds', '--num_rounds', type=int, default=100, required=False)
     parser.add_argument('-epochs', '--num_epochs', type=int, default=3, required=True) # we need this param in test phase
     parser.add_argument('-ncpus', '--num_cpus', type=int, default=8)
-    parser.add_argument('-test', '--test', type=str, required=False, help='Pass model_name here (e.g: lstm_0.001_0.005)')
+    # parser.add_argument('-test', '--test', type=str, required=False, help='Pass model_name here (e.g: lstm_0.001_0.005)')
     # parser.add_argument('--test', action='store_true', required=False)
 
     return vars(parser.parse_args())
 
-def run(data_dict:dict[str, dict[str, list[np.ndarray]]], input_dict:dict, model_dir:str, is_multi:bool=False) -> dict[str:float]:
+def run(
+        data_dict:dict[str, dict],
+        input_dict:dict,
+        model_dir:str,
+        list_id_train:list[int],
+        list_id_val:list[int],
+        list_id_test:list[int]
+    ) -> dict[str:float]:
     """train model with given hyper-param in `input_dict`
 
     Args:
-        data_dict (dict[str, dict[str, list[np.ndarray]]]): dictionary containing all tasks
+        data_dict (dict[str, dict]): dictionary containing all tasks
         input_dict (dict): hyper-param
         model_dir (str): directory to save model and its log
-        is_multi (bool, optional): if True, shuffle tasks. Defaults to False.
 
     Returns:
         metric_dict_: a dictionary containing all metrics for CLF or REG problem
     """
-    list_id = set(data_dict.keys())
-    if is_multi:
-        # only apply for multi_fx
-        # choose randomly 50% clients for training, 25% for validating and 25% for testing
-        list_id_train = random.sample(sorted(list_id), len(list_id)//2)
-        list_id_val = random.sample(sorted(list_id.difference(set(list_id_train))), len(list_id)//4)
-        list_id_test = list(list_id.difference(set(list_id_train).union(list_id_val)))
-    else:
-        # bởi vì các dataset khác không multi
-        # nên phải đảm bảo tính thứ tự trong huấn luyện
-        list_id_train = list_id[:int(len(list_id)*0.5)]
-        list_id_val = list_id[int(len(list_id)*0.5):int(len(list_id)*0.75)]
-        list_id_test = list_id[int(len(list_id)*0.75):]
-
     tf.config.threading.set_inter_op_parallelism_threads(input_dict['num_cpus'])
     tf.config.threading.set_intra_op_parallelism_threads(input_dict['num_cpus'])
 
     # get model
     maml = MAML(
         data_dict=data_dict,
+        output_shape=input_dict['output_shape'],
         based_model=input_dict['based_model'],
         inner_lr=input_dict['inner_learning_rate'],
         outer_lr=input_dict['outer_learning_rate'],
@@ -104,28 +97,37 @@ if __name__ == '__main__':
 
     # config data
     dataset = MULTI_FX
-    dataloader = DataLoader(dataset=dataset, look_back=input_dict['look_back'], mode=input_dict['mode'])
+    dataloader = DataLoader(dataset=dataset, look_back=input_dict['look_back_window'], mode=input_dict['mode'], is_multi='multi' in dataset)
 
     # specify model_dir to save model and its log
     pretrained_dir = os.path.join(PRETRAINED_DIR, dataset)
     model_folder = f"{input_dict['based_model']}_{input_dict['look_back_window']}_{input_dict['inner_learning_rate']}_{input_dict['outer_learning_rate']}"
     model_dir = os.path.join(pretrained_dir, model_folder)
 
-    # create a folder in `pretrained_dir` named model_name
+    # create pretrained dir for dataset
+    if not os.path.exists(pretrained_dir):
+        os.mkdir(pretrained_dir)
+    # create a folder in `pretrained_dir` named model_dir to save model for attributes
     if not os.path.exists(model_dir):
         os.mkdir(model_dir)
 
     summary = {}
     for idx, label in enumerate(dataloader.columns):
-        # get data and the given label
         print(f'\nPredict on label {idx+1}/{len(dataloader.columns)}\n')
+
+        # get data and the given label
         data_dict = dataloader.get_multi_data(label)
 
+        # split train, val, test
+        list_id_train, list_id_val, list_id_test = dataloader.get_list_id(list(data_dict.keys()))
+
         # meta-train, then meta-test and obtain metrics
-        metric_dict = run(data_dict, input_dict, model_dir, 'multi' in dataset)
+        metric_dict = run(data_dict, input_dict, model_dir, list_id_train, list_id_val, list_id_test)
 
         # log for summary a model
         add_summary_log(summary, label, metric_dict)
 
     # write summary of models
     write_summary_log(summary, model_dir)
+
+# python -m execute.main -window 10 -model lstm_cnn -mode classification -out_shape 2 -inner_lr 0.001 -outer_lr 0.0055 -bt_size 3 -rounds 3 -epochs 3 -ncpus 8
