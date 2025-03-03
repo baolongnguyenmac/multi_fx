c955b7a85c6420a1d70dee3550eba3be462005f8
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2025-03-03 13:46:14 +0900

    paper + thesis

--- doc/paper/clean.sh
+++
@@ -1,4 +0,0 @@
-rm *.aux *.blg *.bbl *.fdb_latexmk *.lof *.lot *.out *.fls *.toc *.gz *.xml *.log
-rm Chapters/*.aux
-rm Appendix/*.aux
-rm *blx.bib
---
+++ doc/paper/els-cas-templates/clean.sh
@@ -0,0 +1,3 @@
+rm *.aux *.blg *.bbl *.fdb_latexmk *.lof *.lot *.out *.fls *.toc *.gz *.xml *.log *.abs *.bst
+rm *.cpt
+rm main-blx.bib
